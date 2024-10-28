5b353ae5bf0ceb726012e884cd0c1b35ea5730ee
Unwana Udo <unwana.udo@flo-dojo.com>
2024-10-28 16:48:32 -0600

    changed code because of existing resources

--- main.tf
+++ main.tf
@@ -1,58 +1,48 @@
 # Configure the AWS provider
 provider "aws" {
-  region = "ca-central-1"  # Set the AWS region (central)
+  region = "ca-central-1"
 }
 
-# Create 2 more public subnets in the VPC
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id        = var.vpc_id
-  cidr_block    = var.public_subnet_cidrs[0]
-  availability_zone = var.availability_zone[0]
-
-  tags = {
-    Name = "publicSubnet1"
+# Reference existing public and private subnets instead of creating them
+data "aws_subnet" "public_subnet_1" {
+  filter {
+    name   = "cidrBlock"
+    values = [var.public_subnet_cidrs[0]]
   }
+  vpc_id = var.vpc_id
 }
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id        = var.vpc_id
-  cidr_block    = var.public_subnet_cidrs[1]
-  availability_zone = var.availability_zone[1]
-
-  tags = {
-    Name = "publicSubnet2"
+data "aws_subnet" "public_subnet_2" {
+  filter {
+    name   = "cidrBlock"
+    values = [var.public_subnet_cidrs[1]]
   }
+  vpc_id = var.vpc_id
 }
 
-# Create 2 private subnets in the VPC
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = var.vpc_id
-  cidr_block        = var.private_subnet_cidrs[0]  # Use the first private CIDR block
-  availability_zone = var.availability_zone[0]
-
-  tags = {
-    Name = "privateSubnet1"
+data "aws_subnet" "private_subnet_1" {
+  filter {
+    name   = "cidrBlock"
+    values = [var.private_subnet_cidrs[0]]
   }
+  vpc_id = var.vpc_id
 }
 
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = var.vpc_id
-  cidr_block        = var.private_subnet_cidrs[1]  # Use the second private CIDR block
-  availability_zone = var.availability_zone[1]
-
-  tags = {
-    Name = "privateSubnet2"
+data "aws_subnet" "private_subnet_2" {
+  filter {
+    name   = "cidrBlock"
+    values = [var.private_subnet_cidrs[1]]
   }
+  vpc_id = var.vpc_id
 }
 
-# Create public route table
+# Public route table configuration
 resource "aws_route_table" "public_route_table" {
   vpc_id = var.vpc_id
 
-  # Route all traffic to Internet gateway
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = var.internet_gateway_id  # Use the variable for the Internet Gateway ID
+    gateway_id = var.internet_gateway_id
   }
 
   tags = {
@@ -60,7 +50,7 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-# Create a custom route table for private subnets (no internet access)
+# Private route table configuration (no internet access)
 resource "aws_route_table" "private_route_table" {
   vpc_id = var.vpc_id
 
@@ -69,65 +59,45 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
-# Associate the private subnets with the custom route table
+# Associate existing private subnets with private route table
 resource "aws_route_table_association" "private_subnet_1_association" {
-  subnet_id      = aws_subnet.private_subnet_1.id
+  subnet_id      = data.aws_subnet.private_subnet_1.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
 resource "aws_route_table_association" "private_subnet_2_association" {
-  subnet_id      = aws_subnet.private_subnet_2.id
+  subnet_id      = data.aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
-# Associate the public subnets with the public route table
+# Associate existing public subnets with the public route table
 resource "aws_route_table_association" "public_subnet_1_association" {
-  subnet_id      = aws_subnet.public_subnet_1.id
+  subnet_id      = data.aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table_association" "public_subnet_2_association" {
-  subnet_id      = aws_subnet.public_subnet_2.id
+  subnet_id      = data.aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP traffic"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Allow traffic on the web server port"
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web-sg"
-  }
+# Reference existing security group instead of creating a new one
+data "aws_security_group" "web_sg" {
+  id = var.security_group_id
 }
 
+# Launch EC2 instance in public subnet
 resource "aws_instance" "web_server" {
-  ami                    = "ami-0208b77a23d891325"  # You can also make this an input variable if needed
+  ami                    = "ami-0208b77a23d891325"
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.public_subnet_1.id
-  vpc_security_group_ids = [var.security_group_id]
+  subnet_id              = data.aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [data.aws_security_group.web_sg.id]
   associate_public_ip_address = true
   key_name               = var.key_pair_name
 
   # User data script for the web server
   user_data = <<-EOF
     #!/bin/bash
-    # Install BusyBox
     sudo yum install -y busybox
     echo "<h1>Welcome to tfd'82</h1>" > index.html
     nohup busybox httpd -f -p 8080 &
--- variables.tf
+++ variables.tf
@@ -1,47 +1,45 @@
 # Define variables for common values
 variable "vpc_id" {
-    description = "Default VPC ID tfd-default"
-    default     = "vpc-06fd63ceb391a236d"
+  description = "The VPC ID for the resources"
+  default     = "vpc-06fd63ceb391a236d"
 }
 
 variable "availability_zone" {
-    description = "List of availability zones"
-    default     = ["ca-central-1a", "ca-central-1b"]
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["ca-central-1a", "ca-central-1b"]
 }
 
 variable "public_subnet_cidrs" {
-    description = "List of CIDR blocks for public subnets"
-    default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
+  type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "internet_gateway_id" {
-  description = "The ID of the Internet Gateway"
+  description = "The ID of the Internet Gateway for the VPC"
   default     = "igw-00e5c0a677918ad8e"
 }
 
 variable "security_group_id" {
-  description = "The ID of the security group"
+  description = "The ID of the existing security group to be used for the EC2 instance"
   default     = "sg-0e3ff8f5108fcb7ae"
 }
 
 variable "key_pair_name" {
-  description = "The name of the SSH key pair"
+  description = "The name of the SSH key pair for EC2 access"
   default     = "terraformkeypair1909"
 }
 
 variable "server_port" {
-  description = "The port on which the web servers will listen"
+  description = "The port on which the web server will listen"
   default     = 8080
 }
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "The secret access key for AWS"
-  type        = string
-  sensitive   = true  # Mark this as sensitive to avoid logging it
-}
-
+# Remove sensitive AWS credentials from variables.tf if using environment variables
